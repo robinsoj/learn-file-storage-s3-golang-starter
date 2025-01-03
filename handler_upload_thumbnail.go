1f0420e7e100885e51377b5ec931739219cf9be4
robinsoj <robinsoj1228@gmail.com>
2025-01-02 16:06:07 -0800

    After lesson 1.5

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"time"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
 	"github.com/google/uuid"
 )
 
@@ -28,10 +31,58 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = (10 << 20)
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't Parse multipart form", err)
+		return
+	}
+
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't find the thumbnail", err)
+		return
+	}
+	ct := header.Header.Get("Content-Type")
+	filecontents, err := io.ReadAll(file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to read in the file", err)
+		return
+	}
+	meta, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "User does not own the video", err)
+		return
+	}
+	tn := thumbnail{
+		data:      filecontents,
+		mediaType: ct,
+	}
+	videoThumbnails[meta.ID] = tn
+
+	baseUrl := "http://localhost:" + cfg.port + "/api/"
+	vUrl := baseUrl + "video/" + videoID.String()
+	thUrl := baseUrl + "thumbnails/" + videoID.String()
+	videoItem := database.Video{
+		ID:           meta.ID,
+		CreatedAt:    meta.CreatedAt,
+		UpdatedAt:    time.Now(),
+		VideoURL:     &vUrl,
+		ThumbnailURL: &thUrl,
+		CreateVideoParams: database.CreateVideoParams{
+			Title:       meta.Title,
+			Description: meta.Description,
+			UserID:      userID,
+		},
+	}
+	err = cfg.db.UpdateVideo(videoItem)
+	if err != nil {
+		respondWithError(w, http.StatusUnauthorized, "Update call failed", err)
+		return
+	}
+	fmt.Println(videoItem)
+	respondWithJSON(w, http.StatusOK, videoItem)
 }
